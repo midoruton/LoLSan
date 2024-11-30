239ce4cdb3b9965ed87aec70eacd05f9b9e5092e
midoruton <midoruton0906@gmail.com>
2024-11-30 22:11:40 +0900

    try_lock_ownedを使うように修正

--- src-tauri/src/app/state.rs
+++ src-tauri/src/app/state.rs
@@ -1,7 +1,6 @@
-use tauri::{command, State};
 use futures::lock::Mutex;
 use std::sync::Arc;
 #[derive(Default)]
 pub struct AppState {
-   pub getting_liveclient_data_loop_mutex : Arc<Mutex<()>>, // 排他制御用の Mutex
+   pub liveclinet_data_access_mutex : Arc<Mutex<()>>, // 排他制御用の Mutex
 }
