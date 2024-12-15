ae6b360eea79ea3cb5c81df41f312e55d220521c
midoruton <midoruton0906@gmail.com>
2024-12-16 04:00:55 +0900

    LeagueClientのグローバルステートを追加

--- src-tauri/src/app/state.rs
+++ src-tauri/src/app/state.rs
@@ -1,6 +1,14 @@
+use derive_more::derive::Display;
 use futures::lock::Mutex;
 use std::sync::Arc;
 #[derive(Default)]
 pub struct AppState {
    pub liveclinet_data_access_mutex : Arc<Mutex<()>>, // 排他制御用の Mutex
+   pub league_client_state: Arc<Mutex<LeagueClientState>>,
+}
+#[derive(Default,Display)]
+pub enum LeagueClientState{
+    Connected,
+    InGame(Arc<serde_json::Value>),
+    #[default]Disconnected,
 }
